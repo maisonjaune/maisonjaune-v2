cfe0bbc71fa7371d4bba9644860f6ec9f558ab92
Gwennael Jean <gwennael.jean@gmail.com>
2022-11-15 14:32:29 +0100

    feat(developpement): installation de "symfony/profiler-pack"

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Sonata\BlockBundle\SonataBlockBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
