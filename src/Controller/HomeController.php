8ebefc8bcce32dede8386d3834187af6e6524164
Gwennael Jean <gwennael.jean@gmail.com>
2022-11-18 21:12:29 +0100

    feat(node): création du Provider PostProvider

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/', name: 'app_home')]
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function __invoke(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
