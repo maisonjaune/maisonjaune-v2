6dadbf913801675170e929c32ed2395fbe28c579
Gwennael Jean <gwennael.jean@gmail.com>
2022-11-28 18:02:18 +0100

    feat(parameters): création des entités "Football\Club" et "Football\Team"

---
+++ src/Entity/Football/Club.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity\Football;
+
+use App\Repository\Football\ClubRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClubRepository::class)]
+class Club
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 150)]
+    private ?string $name = null;
+
+    #[ORM\OneToMany(mappedBy: 'club', targetEntity: Team::class, orphanRemoval: true)]
+    private Collection $teams;
+
+    public function __construct()
+    {
+        $this->teams = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Team>
+     */
+    public function getTeams(): Collection
+    {
+        return $this->teams;
+    }
+
+    public function addTeam(Team $team): self
+    {
+        if (!$this->teams->contains($team)) {
+            $this->teams->add($team);
+            $team->setClub($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTeam(Team $team): self
+    {
+        if ($this->teams->removeElement($team)) {
+            // set the owning side to null (unless already changed)
+            if ($team->getClub() === $this) {
+                $team->setClub(null);
+            }
+        }
+
+        return $this;
+    }
+}
